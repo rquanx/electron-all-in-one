c306ae99ec3e1d442f1cbca078bc53cf75139b62
rquanx <runquant@gmail.com>
2025-02-19 22:28:15 +0800

    feat: electron-unhandled

--- src/main/init.ts
+++ src/main/init.ts
@@ -3,6 +3,7 @@ import log from 'electron-log/main'
 import { join } from 'path'
 import dayjs from 'dayjs'
 import util from 'node:util'
+import unhandled from 'electron-unhandled'
 
 debug()
 log.initialize()
@@ -14,3 +15,9 @@ log.transports.file.format = ({ data, level, message }) => {
   const text = util.format(...data)
   return [dayjs(message.date).format('YYYY-MM-DD HH:mm:ss'), `[${level}]`, `{${btoa(text)}}`]
 }
+
+
+unhandled({
+  logger: (error) => log.error(error),
+  showDialog: true
+})
